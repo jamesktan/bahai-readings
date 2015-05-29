0ff4fd21292221a2eda91c43132c2f6b392f7350
James Tan <jamesktan@gmail.com>
2015-05-30 02:25:47 +0300

    initial project commit

---
+++ app/BahaiReadings/BahaiReadings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BahaiReadings
+//
+//  Created by James Tan on 5/30/15.
+//  Copyright (c) 2015 Karr Winn Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
