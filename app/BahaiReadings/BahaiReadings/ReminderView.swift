6c740444a07392f5bf004afc858fbbbfc303fb88
James Tan <jamesktan@gmail.com>
2015-06-01 08:10:56 +0300

    Created and connected all the code to the Reminder  view

---
+++ app/BahaiReadings/BahaiReadings/ReminderView.swift
@@ -0,0 +1,69 @@
+//
+//  ReminderView.swift
+//  BahaiReadings
+//
+//  Created by James Tan on 6/1/15.
+//  Copyright (c) 2015 Karr Winn Labs. All rights reserved.
+//
+
+import UIKit
+
+class ReminderView: UIViewController {
+
+  @IBOutlet weak var optionsView: UIView!
+  
+  @IBOutlet weak var notOnOffLabel: UILabel!
+  @IBOutlet weak var onOffDescription: UILabel!
+  @IBOutlet weak var notOnOffSwitch: UISwitch!
+  
+  @IBOutlet weak var buttonMon: UIButton!
+  @IBOutlet weak var buttonTue: UIButton!
+  @IBOutlet weak var buttonWed: UIButton!
+  @IBOutlet weak var buttonThr: UIButton!
+  @IBOutlet weak var buttonFri: UIButton!
+  @IBOutlet weak var buttonSat: UIButton!
+  @IBOutlet weak var buttonSun: UIButton!
+  @IBOutlet weak var buttonEveryDay: UIButton!
+  
+  @IBOutlet weak var buttonMorn: UIButton!
+  @IBOutlet weak var buttonEvening: UIButton!
+  @IBOutlet weak var buttonNoon: UIButton!
+  @IBOutlet weak var buttonAfternoon: UIButton!
+  @IBOutlet weak var buttonMidnight: UIButton!
+  
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+    
+  @IBAction func switchDidChange(sender: UISwitch) {
+    
+  }
+  
+  @IBAction func daySelected(sender: UIButton) {
+    
+  }
+  
+  @IBAction func timeSelected(sender: UIButton) {
+  }
+  
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
