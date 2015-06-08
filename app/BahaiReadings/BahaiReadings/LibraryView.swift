e6c01a636e6a886d0ce2aba0321c08ed19a3c89f
James Tan <jamesktan@gmail.com>
2015-06-08 17:42:07 +0300

    Changed to cylinder

--- app/BahaiReadings/BahaiReadings/LibraryView.swift
+++ app/BahaiReadings/BahaiReadings/LibraryView.swift
@@ -24,7 +24,7 @@ class LibraryView: UIViewController, iCarouselDataSource, iCarouselDelegate {
     
     libraryCarousel.delegate = self
     libraryCarousel.dataSource = self
-    libraryCarousel.type = iCarouselType.InvertedTimeMachine
+    libraryCarousel.type = iCarouselType.Cylinder
   }
 
   override func viewWillAppear(animated: Bool) {
